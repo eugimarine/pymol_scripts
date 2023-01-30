206159f8309c804c258d2e7d49429162ea9e74dd
eugimarine <eugenio.marinelli22@gmail.com>
2023-01-30 13:47:56 +0100

    load_sequences

---
+++ load_sequences.py
@@ -0,0 +1,20 @@
+from pymol import cmd
+
+
+def load_sequences(file, n_sequences):
+    """ file should be in the format of:
+    [SEQUENCE]  [SUBUNIT]   [START]     [END]
+    tab separated values"""
+    i = 0
+    with open(file) as f:
+        for line in f:
+            i += 1
+            seq, chain, start, end = line.split()
+            cmd.select(seq, f'chain {chain} & resi '+start+'-'+end)
+            if i == n_sequences:
+                return
+cmd.extend('load_sequences', load_sequences)
+
+
+
+
